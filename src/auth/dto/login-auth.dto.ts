44f2f839f2b96b15d412d174da1df71fb36823ae
Maks <alisa3d2007@gmail.com>
2023-12-13 12:30:24 +0300

    feat: the login.auth.dto.ts and signup-auth.dto.ts was changed

--- src/auth/dto/login-auth.dto.ts
+++ src/auth/dto/login-auth.dto.ts
@@ -1,9 +1,11 @@
-import { IsString } from 'class-validator'
+import { IsString, MinLength } from 'class-validator'
 
 export class LoginAuthDto {
 	@IsString()
+	@MinLength(2)
 	username: string
 
 	@IsString()
+	@MinLength(3)
 	password: string
 }
--- src/auth/dto/signup-auth.dto.ts
+++ src/auth/dto/signup-auth.dto.ts
@@ -1,12 +1,15 @@
-import { IsString } from 'class-validator'
+import { IsString, MinLength } from 'class-validator'
 
 export class SignUpAuthDto {
 	@IsString()
+	@MinLength(2)
 	username: string
 
+	@MinLength(2)
 	@IsString()
 	name: string
 
+	@MinLength(3)
 	@IsString()
 	password: string
 }
